50a3916d4c73835faabcaef7922667ec9ff31a38
David Tierney <daveshouse44@hotmail.com>
2023-10-17 00:38:00 -0500

    updated nav and ripples, messed with typescript

--- src/data/navData.ts
+++ src/data/navData.ts
@@ -1,11 +1,21 @@
+type iconSide = "left" | "right";
+
 const navData = [
   {
     name: "About me",
     path: "/me/",
+    icon: {
+      name: "ic:outline-account-box",
+      side: "right" as iconSide,
+    },
   },
   {
     name: "Ripples",
     path: "/ripples/",
+    icon: {
+      name: "ic:round-waves",
+      side: "right" as iconSide,
+    },
   },
 ];
 
