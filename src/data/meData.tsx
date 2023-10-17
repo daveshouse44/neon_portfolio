c97099d1eef4499923e306445c12cdea6bffa903
David Tierney <daveshouse44@hotmail.com>
2023-10-17 01:04:15 -0500

    updated about me

---
+++ src/data/meData.tsx
@@ -0,0 +1,20 @@
+const meData = [
+  {
+    discipline: "Web Development",
+    description: "Building and maintaining websites",
+  },
+  {
+    discipline: "Full Stack",
+    description: "End to end application development",
+  },
+  {
+    discipline: "User Driven Designs",
+    description: "Creating dynamic UX / UI experiences",
+  },
+  {
+    discipline: "Back-End Security",
+    description: "Managing data from the server to user",
+  },
+];
+
+export default meData;
