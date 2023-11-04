28573be7a6905ef3e4ea8f936b1b76abf577d0bf
David Tierney <daveshouse44@hotmail.com>
2023-11-03 23:17:09 -0500

    updated footer, added neon colors from map

---
+++ src/data/neonData.tsx
@@ -0,0 +1,33 @@
+const neonData = [
+  {
+    name: "default",
+    hex: "#2cbfdd",
+  },
+  {
+    name: "violet",
+    hex: "#8c00fc",
+  },
+  {
+    name: "blue",
+    hex: "#006cff",
+  },
+
+  {
+    name: "green",
+    hex: "#01fe01",
+  },
+  {
+    name: "yellow",
+    hex: "#fffe37",
+  },
+  {
+    name: "orange",
+    hex: "#ff8600",
+  },
+  {
+    name: "red",
+    hex: "#ed0003",
+  },
+];
+
+export default neonData;
