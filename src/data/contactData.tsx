d8bf8f96bbcd9f55002efde7d8f29ffafdd6f8a8
David Tierney <daveshouse44@hotmail.com>
2023-11-04 00:13:42 -0500

    updated footer and added more data

---
+++ src/data/contactData.tsx
@@ -0,0 +1,34 @@
+const contactData = [
+  {
+    name: "Email",
+    href: "mailto:me@davetierney.dev:",
+    icon: "ph:envelope-simple-bold",
+    class: "h-4 w-4 text-primary transition duration-300 hover:text-neon",
+  },
+  {
+    name: "LinkedIn",
+    href: "https://www.linkedin.com/in/dave-tierney/",
+    icon: "lucide:linkedin",
+    class: "h-4 w-4 text-primary transition duration-300 hover:text-neon",
+  },
+  {
+    name: "GitHub",
+    href: "https://github.com/daveshouse44",
+    icon: "ri:github-line",
+    class: "h-4 w-4 text-primary transition duration-300 hover:text-neon",
+  },
+  {
+    name: "Reddit",
+    href: "https://www.reddit.com/user/daves_house",
+    icon: "ph:reddit-logo-bold",
+    class: "h-4 w-4 text-primary transition duration-300 hover:text-neon",
+  },
+  {
+    name: "Discord",
+    href: "https://discordapp.com/users/daveshouse44#3785",
+    icon: "ph:discord-logo-bold",
+    class: "h-4 w-4 text-primary transition duration-300 hover:text-neon",
+  },
+];
+
+export default contactData;
