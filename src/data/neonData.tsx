f8c77a58100fe843184bb795d67a4cb735a3e8cc
David Tierney <daveshouse44@hotmail.com>
2023-11-05 00:08:05 -0500

    updated color style

--- src/data/neonData.tsx
+++ src/data/neonData.tsx
@@ -3,51 +3,72 @@ const neonData = [
     name: "default",
     hex: {
       input: "checked:bg-[#2cbfdd]",
-      div: "bg-[#2cbfdd]",
+      div: {
+        border: "border-[#2cbfdd]",
+        background: "bg-[#2cbfdd]",
     },
   },
+},
   {
     name: "violet",
     hex: {
       input: "checked:bg-[#8c00fc]",
-      div: "bg-[#8c00fc]",
+      div: {
+        border: "border-[#8c00fc]",
+        background: "bg-[#8c00fc]",
     },
   },
+},
   {
     name: "blue",
     hex: {
       input: "checked:bg-[#006cff]",
-      div: "bg-[#006cff]",
+      div: {
+        border: "border-[#006cff]",
+        background: "bg-[#006cff]",
     },
   },
+},
   {
     name: "green",
     hex: {
       input: "checked:bg-[#01fe01]",
-      div: "bg-[#01fe01]",
+      div: {
+        border: "border-[#01fe01]",
+        background: "bg-[#01fe01]",
     },
   },
+},
   {
     name: "yellow",
     hex: {
       input: "checked:bg-[#fffe37]",
-      div: "bg-[#fffe37]",
+      div: {
+        border: "border-[#fffe37]",
+        background: "bg-[#fffe37]",
     },
   },
+},
   {
     name: "orange",
     hex: {
       input: "checked:bg-[#ff8600]",
-      div: "bg-[#ff8600]",
+      div: {
+        border: "border-[#ff8600]",
+        background: "bg-[#ff8600]",
     },
   },
+},
   {
     name: "red",
     hex: {
       input: "checked:bg-[#ed0003]",
-      div: "bg-[#ed0003]",
+      div: {
+        border: "border-[#ed0003]",
+        background: "bg-[#ed0003]",
     },
   },
+},
 ];
 
 export default neonData;
