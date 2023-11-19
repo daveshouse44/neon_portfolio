f047643c9f2287fe79c46854cb505a9021b56618
David Tierney <daveshouse44@hotmail.com>
2023-11-19 01:46:50 -0600

    updated a bunch

--- src/data/skillData.tsx
+++ src/data/skillData.tsx
@@ -149,22 +149,4 @@ const skillData = [
     ],
   },
 ];
-const skillDataRestaurant = [
-  {
-    discipline: "Web Development",
-    description: "Building and maintaining websites",
-  },
-  {
-    discipline: "Full Stack",
-    description: "End to end application development",
-  },
-  {
-    discipline: "User Driven Designs",
-    description: "Creating dynamic UX / UI experiences",
-  },
-  {
-    discipline: "Back-End Security",
-    description: "Managing data from the server to user",
-  },
-];
-export { skillData, skillDataRestaurant, skillLinks };
+export { skillData, skillLinks };
