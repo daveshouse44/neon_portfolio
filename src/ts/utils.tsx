db60abf3e0b83c793b6f824c9ec33c18e017836b
David Tierney <daveshouse44@hotmail.com>
2023-11-24 00:56:16 -0600

    updated ripples and sorting

--- src/ts/utils.tsx
+++ src/ts/utils.tsx
@@ -14,3 +14,57 @@ export function formatDate(date: string) {
     timeZone: "UTC",
   });
 }
+
+export interface Ripple {
+  frontmatter: {
+    date: string;
+    draft: boolean;
+  };
+  url?: string;
+}
+
+export function formatRipples(
+  ripples: Ripple[],
+  {
+    filterOutDrafts = true,
+    filterOutFutureRipples = true,
+    sortByDate = true,
+    limit,
+  }: {
+    filterOutDrafts?: boolean;
+    filterOutFutureRipples?: boolean;
+    sortByDate?: boolean;
+    limit?: number;
+  } = {},
+): Ripple[] {
+  const filteredRipples = ripples.reduce((acc: Ripple[], ripple: Ripple) => {
+    const { date, draft } = ripple.frontmatter;
+    // filterOutDrafts if true
+    if (filterOutDrafts && draft) return acc;
+
+    // filterOutFuturePosts if true
+    if (filterOutFutureRipples && new Date(date) > new Date()) return acc;
+
+    // add ripple to acc
+    acc.push(ripple);
+
+    return acc;
+  }, []);
+
+  // sortByDate or randomize
+  if (sortByDate) {
+    filteredRipples.sort(
+      (a, b) =>
+        new Date(b.frontmatter.date).getTime() -
+        new Date(a.frontmatter.date).getTime(),
+    );
+  } else {
+    filteredRipples.sort(() => Math.random() - 0.5);
+  }
+
+  // limit if number is passed
+  if (typeof limit === "number") {
+    return filteredRipples.slice(0, limit);
+  }
+  return filteredRipples;
+}
