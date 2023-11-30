a899f26effda9e8cb0e2e22e45c98348db95b18f
David Tierney <daveshouse44@hotmail.com>
2023-11-30 00:59:59 -0600

    still testing work page

--- src/components/ReactTabs.tsx
+++ src/components/ReactTabs.tsx
@@ -1,16 +1,15 @@
-import { useState, useEffect, useRef } from "react";
-import { IcOutlineLocalPizza } from "../icons/react-icons/pizzaIcon";
-
+import React, { useState, useEffect, useRef } from "react";
 interface TabItem {
   title: string;
   content: string;
+  icon: string;
 }
 
 interface TabsProps {
   items: TabItem[];
 }
 
-const TabsComponent: React.FC<TabsProps> = ({ items }) => {
+const ReactTabs: React.FC<TabsProps> = ({ items }) => {
   const [selectedTab, setSelectedTab] = useState(0);
   const firstBtnRef = useRef<HTMLButtonElement | null>(null);
 
@@ -21,45 +20,40 @@ const TabsComponent: React.FC<TabsProps> = ({ items }) => {
   }, []);
 
   return (
-    <>
-      <div className="flex items-center justify-center">
-        <div className="md:flex">
-          <ul className="flex-column space-y mb-4 space-y-4 text-sm font-medium md:mb-0 md:me-4">
-            {items.map((item, index) => (
-              <li>
-                <button
-                  ref={index === 0 ? firstBtnRef : null}
-                  key={index}
-                  onClick={() => setSelectedTab(index)}
-                  className={`inline-flex w-full items-center rounded-lg px-4 py-3 ${
-                    selectedTab === index ? "bg-white" : ""
-                  } `}
-                >
-                  {item.title} goes here
-                </button>
-              </li>
-            ))}
-          </ul>
-          <div className="text-medium w-full rounded-lg bg-background p-6 text-gray-500">
-            {items.map((item, index) => (
-              <div
-                className={`${selectedTab === index ? "" : "hidden"}`}
-                key={index}
-              >
-                <div className="rounded-lg border-2 border-blue-400 p-4">
-                  <h3 className="mb-2 text-lg font-bold text-gray-900">
-                    {item.title}
-                  </h3>
-                  <p className="mb-2">{item.content}</p>
-                  <IcOutlineLocalPizza className="h-10 w-10 text-neon" />
-                </div>
-              </div>
-            ))}
+    <div className="max-w-5xl md:flex">
+      <ul className="flex-column space-y mb-4 space-y-4 text-sm font-medium md:mb-0 md:me-4">
+        {items.map((item, index) => (
+          <li key={index}>
+            <button
+              ref={index === 0 ? firstBtnRef : null}
+              key={index}
+              onClick={() => setSelectedTab(index)}
+              className={`inline-flex w-full items-center rounded-lg px-4 py-3 ${
+                selectedTab === index ? "bg-white" : ""
+              } `}
+            >
+              {item.title} goes here
+            </button>
+          </li>
+        ))}
+      </ul>
+      <div className="text-medium w-full rounded-lg bg-background p-6 text-gray-500">
+        {items.map((item, index) => (
+          <div
+            className={`${selectedTab === index ? "" : "hidden"}`}
+            key={index}
+          >
+            <div className="rounded-lg border-2 border-blue-400 p-4">
+              <h3 className="mb-2 text-lg font-bold text-gray-900">
+                {item.title}
+              </h3>
+              <p className="mb-2">{item.content}</p>
+            </div>
           </div>
-        </div>
+        ))}
       </div>
-    </>
+    </div>
   );
 };
 
-export default TabsComponent;
+export default ReactTabs;
--- src/data/workTabData.tsx
+++ src/data/workTabData.tsx
@@ -3,21 +3,25 @@ const workTabItems = [
     title: "Tab 1",
     content:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores aperiam asperiores doloribus velit dolore magnam ex consectetur fugit earum illum qui similique architecto dolorum, minima enim quidem voluptatibus at nulla deleniti harum! Totam, mollitia quos voluptatem deleniti provident obcaecati rerum.",
+    icon: "ic:sharp-code",
   },
   {
     title: "Tab 2",
     content:
       "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima ipsam repellat molestias provident accusamus libero harum fuga neque. Hic, nobis animi itaque eius perspiciatis fugit voluptatum repudiandae! Consequatur, impedit laborum?",
+    icon: "ic:outline-local-pizza",
   },
   {
     title: "Tab 3",
     content:
       "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rem omnis, sit fugiat asperiores ea iusto error a numquam, illo sint, odio iure veritatis laboriosam eum facere necessitatibus nemo neque nesciunt. Similique quasi consequuntur ea molestiae iusto, saepe ipsum odit beatae repellat quam soluta iste cum atque ratione repudiandae voluptatum a maxime voluptate sint omnis autem? Temporibus aspernatur quidem libero tenetur. Cupiditate quas, tempora qui suscipit ipsam quibusdam soluta iusto temporibus! Eaque autem delectus qui reiciendis, eligendi, perspiciatis, quis suscipit eum cupiditate magni natus fugit numquam repellendus consectetur est tempore rerum.",
+    icon: "bx:cheese",
   },
   {
     title: "Tab 4",
     content:
       "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Impedit ea voluptas dignissimos voluptatibus repudiandae magnam iusto officia aliquam beatae eos ipsum suscipit laboriosam fugiat exercitationem, quas ullam veritatis deleniti incidunt? Ad non id atque voluptatem ea deleniti aliquid dolore illum, assumenda dicta neque expedita, sequi ab nisi veniam quisquam perferendis accusantium, numquam voluptate odio iusto in vero! Amet, dignissimos odit.",
+    icon: "ph:hammer-bold",
   },
 ];
 
