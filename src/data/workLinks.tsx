a0b91c7f625b6a8688099840c5efddbe9adc69db
David Tierney <daveshouse44@hotmail.com>
2023-12-02 01:47:22 -0600

    updated work data

---
+++ src/data/workLinks.tsx
@@ -0,0 +1,46 @@
+type iconSide = "left" | "right";
+
+const workLinks = [
+  {
+    name: "Medical Alley",
+    link: "https://medicalalley.org/",
+    icon: {
+      name: "teenyicons:wordpress-outline",
+      side: "right" as iconSide,
+    },
+  },
+  {
+    name: "Partner Directory",
+    link: "https://partner.medicalalley.org/",
+    icon: {
+      name: "teenyicons:nextjs-outline",
+      side: "right" as iconSide,
+    },
+  },
+  {
+    name: "Earth Hero",
+    link: "https://www.earthhero.org/",
+    icon: {
+      name: "teenyicons:react-outline",
+      side: "right" as iconSide,
+    },
+  },
+  {
+    name: "Starts Resource Tool",
+    link: "https://www.alleyinsights.org/startup-resources",
+    icon: {
+      name: "teenyicons:vue-outline",
+      side: "right" as iconSide,
+    },
+  },
+  {
+    name: "Snake game",
+    link: "https://daveshouse44.github.io/snake_game/",
+    icon: {
+      name: "teenyicons:javascript-outline",
+      side: "right" as iconSide,
+    },
+  },
+];
+
+export default workLinks;
