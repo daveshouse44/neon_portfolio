4c1cbff032e8988cbba176bb9cdce0e08a502fd3
David Tierney <daveshouse44@hotmail.com>
2023-12-07 00:59:21 -0600

    working on me page now

---
+++ src/data/meData.tsx
@@ -0,0 +1,16 @@
+const meData = [
+  {
+    text: [
+      "I take a strategic approach to design and development. I'm a full stack web developer with a passion for creating dynamic user experiences. I'm currently working as a freelance developer and consultant. I'm also a part-time student at the University of Washington, pursuing a degree in Human Centered Design and Engineering. I'm a lifelong learner and I'm always looking for new opportunities to grow and expand my skillset.",
+      "Hello there! My name is Dave and I appreciate you taking the time to check out my portfolio. I'm a full stack web developer with a passion for creating dynamic user experiences. I'm currently working as a freelance developer and consultant. I'm also a part-time student at the University of Washington, pursuing a degree in Human Centered Design and Engineering. I'm a lifelong learner and I'm always looking for new opportunities to grow and expand my skillset. Everything is trash!",
+    ],
+    facts: [
+      "Building and maintaining websites",
+      "End to end application development",
+      "Focusing on user driven designs",
+      "Creating dynamic UX / UI experiences",
+      "Best practices in SEO and accessibility",
+    ],
+  },
+];
+export default meData;
