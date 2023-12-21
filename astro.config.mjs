4f9bd09083d771afd21c6b4a10f98c0812afb710
David Tierney <daveshouse44@hotmail.com>
2023-12-21 01:10:39 -0600

    added sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
-
 import mdx from "@astrojs/mdx";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind(), mdx()]
+  site: 'https://davetierney.dev',
+  integrations: [react(), tailwind(), mdx(), sitemap()]
 });
