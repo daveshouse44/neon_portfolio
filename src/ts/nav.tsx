1e8557535b1bcc464c261c1dc59b1366731bb941
David Tierney <daveshouse44@hotmail.com>
2024-01-09 22:54:31 -0600

    fixed mobile nav, updated links

--- src/ts/nav.tsx
+++ src/ts/nav.tsx
@@ -1,17 +1,20 @@
-const navLinks = document.querySelectorAll("[data-nav-link]");
+document.addEventListener("astro:after-swap", () => {
+  const navLinks = document.querySelectorAll("[data-nav-link]");
 
-navLinks.forEach((link) => {
-  if (link.getAttribute("href") === window.location.pathname) {
-    link.setAttribute("aria-current", "page");
-  }
+  navLinks.forEach((link) => {
+    if (link.getAttribute("href") === window.location.pathname) {
+      link.setAttribute("aria-current", "page");
+    }
+  });
 });
+document.addEventListener("astro:page-load", () => {
+  const navBarMenu = document.getElementById("navbar-default") as HTMLElement;
 
-const navBarMenu = document.getElementById("navbar-default") as HTMLElement;
+  const navToggle = document.querySelector(
+    "[data-collapse-toggle]",
+  ) as HTMLElement;
 
-const navToggle = document.querySelector(
-  "[data-collapse-toggle]",
-) as HTMLElement;
-
-navToggle.addEventListener("click", () => {
-  navBarMenu.classList.toggle("hidden");
+  navToggle.addEventListener("click", () => {
+    navBarMenu.classList.toggle("hidden");
+  });
 });
