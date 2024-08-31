d7de47c38820e0181975858b1737a476fd56998a
David Tierney <daveshouse44@hotmail.com>
2024-08-31 16:33:58 -0500

    updating me page

--- src/data/meData.tsx
+++ src/data/meData.tsx
@@ -1,6 +1,5 @@
 const meData = [
   {
-    header: "Thank you for visiting!",
     text: [
       "I am currently working as a full stack web developer. I have worn many different hats in my time, but I have always had a desire to know how things work. This has driven me throughout my life and various career focuses. I enjoy the challenge of creating something to achieve what I need, or fixing a problem and then things better.",
       "I began my programming journey in 2021, and I am so glad I did. Though my quest for understanding and problem solving nature has always been an asset, it shines in my current focus. I am excited to continue to learn and grow in this field, and I look forward to the challenges ahead. I will always make, fix, take apart, reassemble, and learn. It is what I do.",
