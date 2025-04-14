deba145aa5f34505f45e43fa352d577cacc22173
Dave <daveshouse44@hotmail.com>
2025-04-13 22:21:09 -0500

    major upgrade to version

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
-
+import icon from "astro-icon";
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://davetierney.dev',
-  integrations: [react(), tailwind(), mdx(), sitemap()]
+  site: "https://davetierney.dev",
+  integrations: [react(), tailwind(), mdx(), sitemap(), icon()],
 });
