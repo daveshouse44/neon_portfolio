adf098ee13e0bd9da277709c27ed6894373e8420
Dave <daveshouse44@hotmail.com>
2026-03-15 17:46:29 -0500

    theme fix

--- src/data/neonData.tsx
+++ src/data/neonData.tsx
@@ -4,71 +4,78 @@ const neonData = [
     hex: {
       input: "checked:bg-[#2cbfdd]",
       div: {
-        border: "border-[#2cbfdd]",
-        background: "bg-[#2cbfdd]",
+        border: "border-[#2cbfdd]/75",
+        background: "bg-[#2cbfdd]/60",
+        backgroundHover: "hover:bg-[#2cbfdd]",
+      },
     },
   },
-},
   {
     name: "violet",
     hex: {
       input: "checked:bg-[#8c00fc]",
       div: {
-        border: "border-[#8c00fc]",
-        background: "bg-[#8c00fc]",
+        border: "border-[#8c00fc]/75",
+        background: "bg-[#8c00fc]/60",
+        backgroundHover: "hover:bg-[#8c00fc]",
+      },
     },
   },
-},
   {
     name: "blue",
     hex: {
       input: "checked:bg-[#006cff]",
       div: {
-        border: "border-[#006cff]",
-        background: "bg-[#006cff]",
+        border: "border-[#006cff]/75",
+        background: "bg-[#006cff]/60",
+        backgroundHover: "hover:bg-[#006cff]",
+      },
     },
   },
-},
   {
     name: "green",
     hex: {
       input: "checked:bg-[#01fe01]",
       div: {
-        border: "border-[#01fe01]",
-        background: "bg-[#01fe01]",
+        border: "border-[#01fe01]/75",
+        background: "bg-[#01fe01]/60",
+        backgroundHover: "hover:bg-[#01fe01]",
+      },
     },
   },
-},
   {
     name: "yellow",
     hex: {
       input: "checked:bg-[#fffe37]",
       div: {
-        border: "border-[#fffe37]",
-        background: "bg-[#fffe37]",
+        border: "border-[#fffe37]/75",
+        background: "bg-[#fffe37]/60",
+        backgroundHover: "hover:bg-[#fffe37]",
+      },
     },
   },
-},
   {
     name: "orange",
     hex: {
       input: "checked:bg-[#ff8600]",
       div: {
-        border: "border-[#ff8600]",
-        background: "bg-[#ff8600]",
+        border: "border-[#ff8600]/75",
+        background: "bg-[#ff8600]/60",
+        backgroundHover: "hover:bg-[#ff8600]",
+      },
     },
   },
-},
   {
     name: "red",
     hex: {
       input: "checked:bg-[#ed0003]",
       div: {
-        border: "border-[#ed0003]",
-        background: "bg-[#ed0003]",
+        border: "border-[#ed0003]/75",
+        background: "bg-[#ed0003]/60",
+        backgroundHover: "hover:bg-[#ed0003]",
+      },
     },
   },
-},
 ];
 
 export default neonData;
